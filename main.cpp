518655d1be9a571c561845aed8fd6f59c4ac399c
DannielMata <danniel1020@gmail.com>
2023-08-25 00:13:57 +0000

    agrego printf

--- main.cpp
+++ main.cpp
@@ -7,10 +7,28 @@ int main()
 
     DigitalOut alarmLed(LED1);
 
+    /*
+    al principio, no pudimos ver el led encendido porque
+    tuvimos problemas con la conexión (COM a GND), 
+    gracias a esta parte nos dimos cuenta de que COM
+    debía estar a 3.3V 
+    */
     gasDetector.mode(PullDown);
 
     alarmLed = OFF;
-
+    /*
+    //agregamos línea de printf, para probarlo abrimos CoolTerm
+    le dimos connect y luego pudimos observar que al resetear
+    muestra de nuevo esta linea, ya que no se encuentra detro
+    del loop "while true"
+    */
+    printf("%s\n", "Hello World"); 
+    
+    /*
+    en esta parte probamos los break points del debug
+    y veíamos en qué parte del código encendía o no
+    de acuerdo al estado del switch
+    */
     while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
@@ -21,3 +39,10 @@ int main()
         }
     }
 }
+
+/*
+pudimos observar que con el keil cloud no hizo falta
+cargar el código "arrastrando" el archivo bin al
+dispositivo conectado ya que este cargaba el código
+presionando run project
+*/
